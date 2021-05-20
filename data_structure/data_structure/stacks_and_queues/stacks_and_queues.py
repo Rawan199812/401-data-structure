9f04f9edc3264c0a92d023cb95a869502b4df525
Rawan199812 <rawan.13579@icloud.com>
2021-05-21 00:55:57 +0300

    add queue

--- data_structure/data_structure/stacks_and_queues/stacks_and_queues.py
+++ data_structure/data_structure/stacks_and_queues/stacks_and_queues.py
@@ -5,8 +5,8 @@ class EmptySatckException(Exception):
 
 class EmptyQueueException(Exception):
     def __init__(self):
-        # print("This Queue still empty") 
-        return "This Queue still empty"
+        print("This Queue still empty") 
+        # return "This Queue still empty"
 
 class Node:
     def __init__(self, value = None):
@@ -35,8 +35,8 @@ class Stack:
 
         if not self.is_empty():
             result = self.top.value
-            temp = self.top.next
-            self.top = temp
+            self.top = self.top.next
+             
             return result
         else:
             raise EmptySatckException
@@ -57,17 +57,83 @@ class Stack:
             current = current.next
         return result
 
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+
+    def enqueue(self, value):
+        """Method takes any value as argument and adds node with that value to back of queue"""
+        node = Node(value)
+        if not self.front:
+            """If we have empty queue then the first node will be the front and the rear"""
+            self.front = node
+            self.rear = node
+        else:
+            """make the last node points to the new"""
+            self.rear.next = node
+            """make the new node is the back of the queue"""
+            self.rear = self.rear.next
+    def dequeue(self):
+        """Method removes node from front of queue (no args) and returns the node's data"""
+        if not self.is_empty():
+            result = self.front.value
+            self.front = self.front.next
+            return result
+        else:
+            raise EmptyQueueException
+    def peek(self):
+        """Method (no args) returns the value of the node located in the front of the queue"""
+        if not self.is_empty():
+            return self.front.value
+        else:
+            raise EmptyQueueException    
+
+    def is_empty(self):
+        """Method (no args) returns a boolean indicating whether or not the queeu is empty"""
+        if self.front == None:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        result = ""
+        current = self.front
+        while current:
+            result += f"({current.value}) => "
+            current = current.next
+        return result
+    
+
+
+
+        
+
         
     
 stack = Stack()
-stack.push(" first node ")       
-stack.push(" 2 node ")  
-stack.push(" 3 node ")  
-stack.push(" 4 node ")       
-print(stack)
+# stack.push(" first node ")       
+# stack.push(" 2 node ")  
+# stack.push(" 3 node ")  
+# stack.push(" 4 node ")       
+# print(stack)
 # stack.pop() 
+# print(stack)
 # print(stack.pop())
 # print(stack.peek())
 # print(stack.is_empty())
-print(stack)
+# print(stack)
+
+queue = Queue()
+ 
+# queue.enqueue(" first node ")
+# queue.enqueue(" 2 node ")  
+# queue.enqueue(" 3 node ")  
+# queue.enqueue(" 4 node ") 
+# print(queue)
+# queue.dequeue()
+# print(queue)
+# print(queue.peek())
+
+# print(queue.is_empty())
 
--- tests/test_stacks_and_queues.py
+++ tests/test_stacks_and_queues.py
@@ -1,7 +1,8 @@
 import pytest
-from data_structure.data_structure.stacks_and_queues.stacks_and_queues import Node, Stack, EmptySatckException, EmptyQueueException
+from data_structure.data_structure.stacks_and_queues.stacks_and_queues import Node, Queue, Stack, EmptySatckException, EmptyQueueException
 
 stack = Stack()
+queue = Queue()
 def test_version():
     pass
 
@@ -21,7 +22,7 @@ def test_push_multiple_values_onto_a_stack():
 
 def test_pop_off_the_stack():
     expected = "(4 node) => (3 node) => (2 node) => (first node) => "
-    stack.push("(5 node)")
+    stack.push("5 node")
     stack.pop()
     actual = str(stack)
     assert actual == expected
@@ -49,6 +50,52 @@ def  test_peek_on_empty_stack_raises_exception(empty_STK):
     with pytest.raises(EmptySatckException):
          empty_STK.peek()
 
+# =========================== Queue tests ========================
+def test_enqueue_one_into_a_queue():
+    expected = "(first node) => "
+    queue.enqueue("first node")
+    actual = str(queue)
+    assert actual == expected
+
+def test_enqueue_multiple_values_into_a_queue():
+    expected = "(first node) => (2 node) => (3 node) => (4 node) => "
+    queue.enqueue("2 node")
+    queue.enqueue("3 node")
+    queue.enqueue("4 node")
+    actual = str(queue)
+    assert actual == expected
+
+def test_enqueue_off_the_queue():
+    expected = "(2 node) => (3 node) => (4 node) => (5 node) => "
+    queue.enqueue("5 node")
+    queue.dequeue()
+    actual = str(queue)
+    assert actual == expected
+
+def  test_peek_into_the_queue():
+    expected = "2 node"
+    actual = queue.peek()
+    assert actual == expected
+
+
+def test_empty_a_queue_after_multiple_dequeues(Qu):
+    Qu.dequeue()
+    Qu.dequeue()
+    Qu.dequeue()
+    Qu.dequeue()
+    with pytest.raises(EmptyQueueException):
+         Qu.dequeue()
+
+def  test_dequeue_on_empty_queue_raises_exception(empty_Qu):
+
+    with pytest.raises(EmptyQueueException):
+         empty_Qu.dequeue()
+
+def  test_peek_on_empty_queue_raises_exception(empty_Qu):
+
+    with pytest.raises(EmptyQueueException):
+         empty_Qu.peek()
+
 
 
 
@@ -74,3 +121,21 @@ def empty_STK():
     empty_stack.push('first node')
     empty_stack.pop()
     return empty_stack
+
+@pytest.fixture
+def Qu():
+    queue = Queue()
+    queue.enqueue('first node')
+    queue.enqueue("2 node")
+    queue.enqueue("3 node")
+    queue.enqueue("4 node")
+
+    return queue
+
+
+@pytest.fixture
+def empty_Qu():
+    empty_queue= Queue()
+    empty_queue.enqueue('first node')
+    empty_queue.dequeue()
+    return empty_queue
