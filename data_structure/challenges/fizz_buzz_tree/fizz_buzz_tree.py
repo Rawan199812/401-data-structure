6999a243e73171d45fd3979f73e70549251270a4
Rawan199812 <rawan.13579@icloud.com>
2021-06-01 23:44:19 +0300

    first commit

--- data_structure/challenges/fizz_buzz_tree/fizz_buzz_tree.py
+++ data_structure/challenges/fizz_buzz_tree/fizz_buzz_tree.py
@@ -4,18 +4,19 @@ from data_structure.data_structure.tree.tree import Node, BinaryTree
 #     tree = BinarySearchTree()
 #     pass
 def FizzBuzzTree(tree):
-    new_tree = tree
-    if new_tree.root != None:
+    """
+    k-ary : can have any number of nodes
+    """
+    if tree.root != None:
         pass
 
     else:
-        new_tree.root =Node('Tree is Empty')
-        return new_tree
-tree = BinaryTree("1")
-tree.root.left = Node("2")
-tree.root.right = Node("3")
-tree.root.left.left = Node("4")
-tree.root.left.right = Node("5")
-tree.root.right.left = Node("6")
-tree.root.right.right = Node("8")
-print(tree.breadth_first_traversal(FizzBuzzTree(tree)))
+        tree.root =Node('Tree is Empty')
+        return tree
+# tree = BinaryTree("1")
+# tree.root.left = Node("2")
+# tree.root.right = Node("3")
+# tree.root.left.left = Node("4")
+# tree.root.left.right = Node("5")
+# tree.root.right.left = Node("6")
+# tree.root.right.right = Node("8")
