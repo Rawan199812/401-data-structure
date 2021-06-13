230d1e7d0a5ff2dfff432bd102de3b4ed2f69ac7
Rawan199812 <rawan.13579@icloud.com>
2021-06-14 01:36:50 +0300

    done

---
+++ tests/test_sort.py
@@ -0,0 +1,7 @@
+import pytest
+from data_structure.data_structure.sort.sort import insertion_sort
+
+def test_insertion_sort_integers_values_one():
+    expected = [4, 8, 15, 16, 23, 42]
+    actual = insertion_sort([8,4,23,42,16,15])
+    assert actual == expected
