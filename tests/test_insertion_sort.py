9fd18ae809d301fca739e447540cb3e46660b518
Rawan199812 <rawan.13579@icloud.com>
2021-06-14 23:01:10 +0300

    fix naming

--- tests/test_insertion_sort.py
+++ tests/test_insertion_sort.py
@@ -1,5 +1,5 @@
 import pytest
-from data_structure.data_structure.sort.sort import insertion_sort
+from data_structure.data_structure.insertion_sort.insertion_sort import insertion_sort
 
 def test_insertion_sort_integers_values_one():
     expected = [4, 8, 15, 16, 23, 42]
