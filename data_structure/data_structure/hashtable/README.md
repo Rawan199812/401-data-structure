d2ac2b0e6c719e63316e192f0c0a7ef2a5dc208f
Rawan199812 <rawan.13579@icloud.com>
2021-06-17 23:10:20 +0300

    done without get

---
+++ data_structure/data_structure/hashtable/README.md
@@ -0,0 +1,3 @@
+
+
+hash() : method results in the least number of collisions, meaning, No two sets of information should have the same hash values.
---
+++ data_structure/data_structure/hashtable/hashtable.py
@@ -0,0 +1,47 @@
+from data_structure.data_structure.linked_list.linked_list import LinkedList, Node
+
+class Hashtable:
+    def __init__(self, size = 1024):
+        self.size = size
+        self.buckets = [None] * self.size 
+    def add(self,key,value):
+        """
+        takes in both the key and value. This method should hash the key, and add the key and value pair to the table, handling collisions as needed.
+        """
+
+        index = self.hash(key)
+        if not self.buckets[index]:
+            self.buckets[index] = LinkedList()
+        return self.buckets[index].append([key,value])
+
+
+        
+    def get(self,key):
+        return "value"
+    def contains(self,key):
+        """
+        takes in the key and returns a boolean, indicating if the key exists in the table already.
+        """
+        index = self.hash(key)
+        if self.buckets[index]:
+            return True
+        return False
+
+    def hash(self,key):
+        """
+         takes in an arbitrary key and returns an index in the collection.
+        """
+        sum = 0
+        for char in key:
+            sum+= ord(char)  ## ord() will return Unicode equivalence of the string 
+        hash_key = (sum*23)%(self.size)
+        return hash_key
+
+
+hash1 = Hashtable()
+print(hash1.hash("A"))
+hash1.add("A","first")
+hash1.add("A","first-b")
+
+# print(str(hash1.buckets))
+print(hash1.contains("A"))
