01393eea03505fb06574fef19043729037ca4568
Rawan199812 <rawan.13579@icloud.com>
2021-06-20 19:53:25 +0300

    add tests

--- data_structure/challenges/repeated_word/repeated_word.py
+++ data_structure/challenges/repeated_word/repeated_word.py
@@ -3,7 +3,8 @@ from collections import Counter , OrderedDict
 
   
 def repeated_word(string): 
-    reg = re.findall('[\w\']+',string.lower()) # findall:If one or more groups are present in the pattern, return a  list of tuples 
+    reg = re.findall('[\w\']+',string.lower()) 
+    # findall:If one or more groups are present in the pattern, return a  list of tuples 
     counted = Counter(reg) 
     # for i in reg:
     #     counted = OrderedDict.popitem(i)
@@ -18,9 +19,9 @@ def repeated_word(string):
       if counted[key]>1: 
         print (key) 
         return key
-        
+
 
 string = "Once upon a time, there was a brave princess who..."
 string2 = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only..."
 string3 = "It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York..."
-repeated_word(string2)
+# repeated_word(string2)
