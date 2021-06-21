b006d26a92f690c326c803c56a373da30426b7aa
Rawan199812 <rawan.13579@icloud.com>
2021-06-21 10:47:38 +0300

    last string fixed

--- data_structure/challenges/repeated_word/repeated_word.py
+++ data_structure/challenges/repeated_word/repeated_word.py
@@ -5,23 +5,28 @@ from collections import Counter , OrderedDict
 def repeated_word(string): 
     reg = re.findall('[\w\']+',string.lower()) 
     # findall:If one or more groups are present in the pattern, return a  list of tuples 
-    counted = Counter(reg) 
-    # for i in reg:
-    #     counted = OrderedDict.popitem(i)
-    #     print(counted)
-    # repeated = {}
-    # for i in reg:
-    #     if counted[i]>1:
-    #         repeated[counted[i]].keys = counted[i]
-    # print(repeated)
 
-    for key in reg:
-      if counted[key]>1: 
-        print (key) 
-        return key
+    ## using Counter
+
+    # counted = Counter(reg) 
+    # for key in reg:
+    #   if counted[key]>1: 
+    #     print (key) 
+    #     return key
+    words = []
+    repeated = ""
+
+    for i in reg: 
+        if i in words: 
+            repeated = i
+            break
+        words.append(i) 
+    print(words)
+    print(repeated)
+    return repeated
 
 
 string = "Once upon a time, there was a brave princess who..."
 string2 = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only..."
 string3 = "It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York..."
-# repeated_word(string2)
+repeated_word(string3)
